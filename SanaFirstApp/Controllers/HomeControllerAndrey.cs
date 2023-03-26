876c7f94bae1c4b2e7d5de53ddd4b61dee937845
Andrii Hretskyi <andreygretzky@gmail.com>
2023-03-27 00:09:04 +0300

    Add project files.

---
+++ SanaFirstApp/Controllers/HomeControllerAndrey.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaFirstApp.Models;
+namespace SanaFirstApp.Controllers
+{
+    [Route("[controller]")]
+    [Route("")]
+    public class HomeControllerAndrey : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            if (name != null)
+            {
+                
+                return View("Views/EmoH/Result.cshtml", new UsenameViewModel() { UsenameView = name });
+
+            }
+
+            return View("Views/EmoH/index.cshtml");
+        }
+    }
+}
